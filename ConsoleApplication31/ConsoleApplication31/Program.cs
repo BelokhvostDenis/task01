4327b005acf2612a06bfba334e03eed0d8121728
BelokhvostDenis <natedrake@mail.ru>
2015-09-19 12:00:24 +0500

    first

---
+++ ConsoleApplication31/ConsoleApplication31/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication30
+{
+
+    class Program
+    {
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string word = "privet kak dela";
+            word = Clipboard.GetText();
+            Console.WriteLine(word);
+
+
+            string[] split = word.Split(new Char[] { ' ', ',', '.', ':', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            IEnumerable<string> sort =
+       from wo in split
+       orderby wo //"ascending" is default
+       select wo;
+            var DistincList = sort.Distinct();
+            foreach (string a in DistincList.Take(1000))
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
